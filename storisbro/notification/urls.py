6df64582ea0191d8eefc2b13955dabd8b7067ce1
OciMM <bekasovmaks20@gmail.com>
2024-06-01 23:07:43 +0300

    fix

--- storisbro/notification/urls.py
+++ storisbro/notification/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import SendNotificationView, NotificationMainAPIView, NotificationVKandEmail, NotificationGetAPIView
+from .views import SendNotificationView, NotificationMainAPIView, NotificationVKandEmail, NotificationGetAPIView, \
+    BulkNotificationCreateAPIView
 
 urlpatterns = [
     path('send-notification/', SendNotificationView.as_view(), name='send-notification'),
     path('send-notification/message/', NotificationMainAPIView.as_view()),
     path('send-notification/message/<str:uid>/', NotificationGetAPIView.as_view()),
     path('send-notification/vk_and_email/<str:uid>/', NotificationVKandEmail.as_view()),
+    path('send-notification/bulk-notifications/', BulkNotificationCreateAPIView.as_view(), name='bulk-notifications'),
 ]
