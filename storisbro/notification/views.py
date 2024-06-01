c1015f27424af5878efe0829734d0514d5833277
OciMM <bekasovmaks20@gmail.com>
2024-06-01 13:50:53 +0300

    fix

--- storisbro/notification/views.py
+++ storisbro/notification/views.py
@@ -39,7 +39,7 @@ class NotificationMainAPIView(APIView):
 
 class NotificationGetAPIView(APIView):
     def get(self, request, uid):
-        notification_model = Notification.objects.all(user=uid) 
+        notification_model = Notification.objects.get(user=uid) 
         serializer = NotificationSerializer(notification_model)
         return Response(serializer.data)
     
