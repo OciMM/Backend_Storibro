767dbd3e4c0fc1b8bfe082554724f02c6e0b5cda
OciMM <bekasovmaks20@gmail.com>
2024-06-01 13:07:50 +0300

    fix

--- storisbro/notification/urls.py
+++ storisbro/notification/urls.py
@@ -4,6 +4,6 @@ from .views import SendNotificationView, NotificationMainAPIView, NotificationVK
 urlpatterns = [
     path('send-notification/', SendNotificationView.as_view(), name='send-notification'),
     path('send-notification/message/', NotificationMainAPIView.as_view()),
-    path('send-notification/message/<str:uid>/', NotificationGetAPIView.as_view()),
+    path('send-notification/message/<int:pk>/', NotificationGetAPIView.as_view()),
     path('send-notification/vk_and_email/<str:uid>/', NotificationVKandEmail.as_view()),
 ]
